5c8ad7ffedfc36754d5c1b95a950291fb56f9de0
Paul Kiernan <paul.kiernan@reddit.com>
2019-06-14 02:44:39 -0700

    Basic 8 channel test

--- nowhere.ino
+++ nowhere.ino
@@ -1,28 +1,99 @@
-/* LED Blink, Teensyduino Tutorial #1
-   http://www.pjrc.com/teensy/tutorial.html
+#include "FastLED.h"
 
-   This example code is in the public domain.
-*/
+// How many leds in your strip?
+#define NUM_LEDS 300
+#define MAX_BRIGHTNESS 255
+#define TEENSY_LED_PIN 13
+#define CHIPSET        WS2813
+#define COLOR_ORDER    GRB
 
-// Teensy 2.0 has the LED on pin 11
-// Teensy++ 2.0 has the LED on pin 6
-// Teensy 3.x / Teensy LC have the LED on pin 13
-const int ledPin = 13;
+#define LED_STRIP0_PIN  2
+#define LED_STRIP1_PIN  14
+#define LED_STRIP2_PIN  7
+#define LED_STRIP3_PIN  8
+#define LED_STRIP4_PIN  6
+#define LED_STRIP5_PIN  29
+#define LED_STRIP6_PIN  21
+#define LED_STRIP7_PIN  5
 
-// the setup() method runs once, when the sketch starts
+CRGB led_strip0[NUM_LEDS];
+CRGB led_strip1[NUM_LEDS];
+CRGB led_strip2[NUM_LEDS];
+CRGB led_strip3[NUM_LEDS];
+CRGB led_strip4[NUM_LEDS];
+CRGB led_strip5[NUM_LEDS];
+CRGB led_strip6[NUM_LEDS];
+CRGB led_strip7[NUM_LEDS];
+
+uint8_t ledBrightness = 255;
+uint8_t direction = -1;
+int period = 10;
+
 
 void setup() {
-  // initialize the digital pin as an output.
-  pinMode(ledPin, OUTPUT);
-}
+    pinMode(TEENSY_LED_PIN, OUTPUT);
 
-// the loop() methor runs over and over again,
-// as long as the board has power
+    // LED Output test
+    FastLED.addLeds<CHIPSET, LED_STRIP0_PIN, COLOR_ORDER>(
+        led_strip0,
+        NUM_LEDS
+    ).setCorrection(TypicalSMD5050);
+    FastLED.addLeds<CHIPSET, LED_STRIP1_PIN, COLOR_ORDER>(
+        led_strip1,
+        NUM_LEDS
+    ).setCorrection(TypicalSMD5050);
+    FastLED.addLeds<CHIPSET, LED_STRIP2_PIN, COLOR_ORDER>(
+        led_strip2,
+        NUM_LEDS
+    ).setCorrection(TypicalSMD5050);
+    FastLED.addLeds<CHIPSET, LED_STRIP3_PIN, COLOR_ORDER>(
+        led_strip3,
+        NUM_LEDS
+    ).setCorrection(TypicalSMD5050);
+    FastLED.addLeds<CHIPSET, LED_STRIP4_PIN, COLOR_ORDER>(
+        led_strip4,
+        NUM_LEDS
+    ).setCorrection(TypicalSMD5050);
+    FastLED.addLeds<CHIPSET, LED_STRIP5_PIN, COLOR_ORDER>(
+        led_strip5,
+        NUM_LEDS
+    ).setCorrection(TypicalSMD5050);
+    FastLED.addLeds<CHIPSET, LED_STRIP6_PIN, COLOR_ORDER>(
+        led_strip6,
+        NUM_LEDS
+    ).setCorrection(TypicalSMD5050);
+    FastLED.addLeds<CHIPSET, LED_STRIP7_PIN, COLOR_ORDER>(
+        led_strip7,
+        NUM_LEDS
+    ).setCorrection(TypicalSMD5050);
+
+    for(int dot = 0; dot < NUM_LEDS; dot++) {
+        led_strip0[dot] = 0xe6194B;
+        led_strip1[dot] = 0xf58231;
+        led_strip2[dot] = 0xffe119;
+        led_strip3[dot] = 0xbfef45;
+        led_strip4[dot] = 0x3cb44b;
+        led_strip5[dot] = 0x42d4f4;
+        led_strip6[dot] = 0x4363d8;
+        led_strip7[dot] = 0x911eb4;
+    }
+    FastLED.setBrightness(MAX_BRIGHTNESS);
+    FastLED.show();
+}
 
 void loop() {
-  digitalWrite(ledPin, HIGH);   // set the LED on
-  delay(100);                  // wait for a second
-  digitalWrite(ledPin, LOW);    // set the LED off
-  delay(100);                  // wait for a second
+    digitalWrite(TEENSY_LED_PIN, HIGH);   // set the LED on
+    delay(period);                  // wait for a second
+    digitalWrite(TEENSY_LED_PIN, LOW);    // set the LED off
+    delay(period);                  // wait for a second
+    ledBrightness += direction;
+    if (ledBrightness == 0){
+      direction = 1;
+    } else if (ledBrightness == MAX_BRIGHTNESS){
+      direction = -1;
+    }
+    for(int dot = 0; dot < NUM_LEDS; dot++) {
+        FastLED.setBrightness(ledBrightness);
+    }
+    FastLED.show();
 }
-
